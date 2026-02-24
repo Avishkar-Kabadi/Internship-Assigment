9ffe2a10d2548f5b06c2d730dc634344a3a862ea
Avishkar Kabadi <razergaming1828@gmail.com>
2026-02-24 21:19:03 +0530

    refactor: remove unused imports and debugging console logs

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 
-import { Route, Routes, Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ isLoggedIn }) => {
     if (!isLoggedIn) {
